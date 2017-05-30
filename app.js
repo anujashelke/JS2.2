2f17c037aa6914f6faaa4676af64569574e205cc
anujashelke <anuja_shelke@outlook.com>
2017-05-30 16:21:32 +0530

    submitted

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+var a = parseInt(prompt("Enter the first value ", "Enter"));
+
+
+var b = parseInt(prompt("Enter the Second value ", "Enter"));
+
+
+var c = parseInt(prompt("Enter the third value ", "Enter"));
+
+
+if(a > b)
+{
+
+document.write("a is geratest and the value is : " + a);
+
+}
+
+
+else if(b > c)
+
+{
+
+document.write("b is geratest and the value is : " + b);
+
+}
+
+
+else
+
+{
+
+document.write("c is geratest and the value is : " + c);
+
+}
